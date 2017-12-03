17dbf2a7684d6ddf185602416adf63ccc2166fde
alkamid <adamkli@gmail.com>
2017-12-03 00:14:03 +0000

    Parse language sections

--- pagepl.py
+++ pagepl.py
@@ -1,21 +1,51 @@
+import re
 from pywikibot import Page
 from pywikibot import Site
 from pywikibot.xmlreader import XmlEntry
 
 
 class PagePL:
-    def __init__(self):
+    def __init__(self, parse=True):
         self.site_plwikt = Site('pl', 'wiktionary')
-        self.language_sections = None
+        self.language_sections = dict()
+        self.regexps = dict()
+        self.regexps['header'] = re.compile(r'(.*?)==', re.DOTALL)
+        self.regexps['lang_section'] = re.compile(r'(== .*?\(\{\{.*?\}\}\) ==.*?)(?=$|[^{{]==)', re.DOTALL)
+
+        if parse:
+            self.find_language_sections()
+
+    def find_language_sections(self):
+        s_header = re.search(self.regexps['header'], self.text)
+        if s_header:
+            self.language_sections['header'] = s_header.group(1)
+
+        s_lang = re.findall(self.regexps['lang_section'], self.text)
+        if s_lang:
+            tempidx = 'abcdefg'
+            for i, l in enumerate(s_lang):
+                self.language_sections[tempidx[i]] = l
+                #self.listLangs.append(LanguageSection(l, self.title))
+        else:
+            raise SectionsNotFound
 
 
 class PagePLWiki(Page, PagePL):
     def __init__(self, title, **kwargs):
+        site_plwikt = Site('pl', 'wiktionary')
+        Page.__init__(self, source=site_plwikt, title=title, **kwargs)
         PagePL.__init__(self)
-        Page.__init__(self, source=self.site_plwikt, title=title, **kwargs)
 
 
 class PagePLXML(XmlEntry, PagePL):
-    def __init__(self, title, **kwargs):
+    def __init__(self, **kwargs):
         PagePL.__init__(self)
         XmlEntry.__init__(self, **kwargs)
+
+
+class SectionsNotFound(Exception):
+    def __init__(self):
+        self.value = 'language sections not found!'
+
+    def __str__(self):
+        return repr(self.value)
--- test/test_plPageParser.py
+++ test/test_plPageParser.py
@@ -6,7 +6,7 @@ def test_meanings():
     test_page.text = test_page.getOldVersion(oldid=5125503)
     assert test_page.title() == 'robić'
 
-    # assert len(test_page.language_sections) == 1
+    assert len(test_page.language_sections) == 2
     # assert test_page.language_sections['polski'].subsections['przykłady'][0].text == \
     #     '\'\'[[w|W]] [[ten|tej]] [[fabryka|fabryce]] [[robić|robią]] [[samochód|samochody]].\'\''
     # assert test_page.language_sections['polski'].subsections['przykłady'][1].number == '(1.1)'
