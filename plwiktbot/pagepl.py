d49cd8f6d06a139658c42fa8184f6d34fbce8ccd
alkamid <adamkli@gmail.com>
2018-11-13 06:32:50 +0000

    Add lang section finding without regexp

--- plwiktbot/pagepl.py
+++ plwiktbot/pagepl.py
@@ -1,4 +1,7 @@
 import re
+from gettext import find
+from typing import Optional, List, Tuple
+
 from pywikibot import Page
 from pywikibot import Site
 from pywikibot.xmlreader import XmlEntry
@@ -9,23 +12,23 @@ class PagePL:
         self.site_plwikt = Site('pl', 'wiktionary')
         self.language_sections = dict()
         self.regexps = dict()
-        self.regexps['header'] = re.compile(r'(.*?)==', re.DOTALL)
-        self.regexps['lang_section'] = re.compile(r'(== .*?\(\{\{.*?\}\}\) ==.*?)(?=$|[^{{]==)', re.DOTALL)
+        self.header = ''
+        if not self.text:
+            self.text = ''
 
         if parse:
             self.find_language_sections()
 
     def find_language_sections(self):
-        s_header = re.search(self.regexps['header'], self.text)
-        if s_header:
-            self.language_sections['header'] = s_header.group(1)
+        header_index = self.text.index('==')
+        self.header = self.text[:header_index]
 
-        s_lang = re.findall(self.regexps['lang_section'], self.text)
-        if s_lang:
-            tempidx = 'abcdefg'
-            for i, l in enumerate(s_lang):
-                self.language_sections[tempidx[i]] = l
-                #self.listLangs.append(LanguageSection(l, self.title))
+        sec_list = find_sections_seq(self.text[header_index:])
+        if sec_list:
+            for head, content in sec_list:
+                lang_start = head.find('({{') + 3
+                lang_end = head.find('}})')
+                self.language_sections[head[lang_start:lang_end]] = Section(content)
         else:
             raise SectionsNotFound
 
@@ -33,8 +36,10 @@ class PagePL:
 class PagePLWiki(Page, PagePL):
     def __init__(self, title, **kwargs):
         site_plwikt = Site('pl', 'wiktionary')
+        parse = kwargs.pop('parse', True)
+        print(kwargs)
         Page.__init__(self, source=site_plwikt, title=title, **kwargs)
-        PagePL.__init__(self)
+        PagePL.__init__(self, parse=parse)
 
 
 class PagePLXML(XmlEntry, PagePL):
@@ -49,3 +54,39 @@ class SectionsNotFound(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+
+class Section:
+    def __init__(self, wikitext: Optional[str]=None, parse=True):
+        self.wikitext = wikitext
+        self.meanings = ''
+
+        if wikitext is None and parse:
+            raise ValueError('In order to parse a section, its wikitext must be provided!')
+
+        if parse:
+            self.parse(wikitext)
+
+    def parse(self, wikitext: str) -> None:
+        meanings_start = wikitext.find('{{znaczenia}}') + len('{{znaczenia}}')
+        meanings_end = wikitext.find('{{odmiana}}')
+        self.meanings = wikitext[meanings_start:meanings_end].strip()
+
+
+def find_sections_seq(text: str) -> List[Tuple[str, str]]:
+    sec_list = []
+
+    start = 0
+    while start != -1:
+        assert text[start:start+2] == '=='
+        header_end = text.find('==', start+2)
+        header = text[start:header_end+2]
+        sec_end = text.find('==', header_end+2)
+        if sec_end == -1:
+            sec = text[header_end+2:]
+        else:
+            sec = text[header_end+2:sec_end]
+        start = sec_end
+        sec_list.append((header, sec))
+
+    return sec_list
