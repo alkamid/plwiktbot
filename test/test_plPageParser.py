caf10862b8b7ba1eeda1e55ac854481154f90fea
alkamid <adamkli@gmail.com>
2018-11-17 11:13:08 +0000

    Adjust test

--- test/test_plPageParser.py
+++ test/test_plPageParser.py
@@ -2,19 +2,19 @@ from plwiktbot.pagepl import PagePLWiki
 
 
 def test_meanings():
-    test_page = PagePLWiki(title='robić')
+    test_page = PagePLWiki(title='robić', parse=False)
     test_page.text = test_page.getOldVersion(oldid=6202869)
     assert test_page.title() == 'robić'
+    test_page.find_language_sections()
 
-    assert len(test_page.language_sections) == 2
-    plsec = test_page.language_sections['polski']
-    plsec.parse()
+    assert len(test_page.language_sections) == 1
+    plsec = test_page.language_sections['język polski']
 
-    assert plsec.pos[0] == 'czasownik przechodni niedokonany'
-    assert plsec.sense[0].id == '1.1'
-    assert plsec.sense[0].wikitext == '[[wykonywać]] [[coś]], [[tworzyć]], [[produkować]], [[przyrządzać]], [[przygotowywać]]'
-    assert plsec.sense[0].pos == plsec.pos[0]
+    assert 'czasownik przechodni niedokonany' in plsec.pos[0].wikitext
+    assert plsec.senses[0].id == '1.1'
+    assert plsec.senses[0].wikitext == '[[wykonywać]] [[coś]], [[tworzyć]], [[produkować]], [[przyrządzać]], [[przygotowywać]]'
+    assert plsec.senses[0].pos == plsec.pos[0]
 
-    assert plsec.sense[6].id == '3.2'
-    assert plsec.sense[6].wikitext == '[[stawać się]] [[jakiś|jakimś]], [[zmieniać się]]<ref name="SJPonline-się" />'
-    assert plsec.sense[6].pos == plsec.pos[2]
+    assert plsec.senses[6].id == '3.2'
+    assert plsec.senses[6].wikitext == '[[stawać się]] [[jakiś|jakimś]], [[zmieniać się]]<ref name="SJPonline-się" />'
+    assert plsec.senses[6].pos == plsec.pos[2]
