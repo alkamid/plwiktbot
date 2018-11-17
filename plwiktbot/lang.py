125c2b331847dc3b246c42d02427dc93e191600c
alkamid <adamkli@gmail.com>
2018-11-17 11:12:21 +0000

    Add part of speech and sense dataclasses

--- plwiktbot/lang.py
+++ plwiktbot/lang.py
@@ -1,11 +1,45 @@
-from typing import Optional
+from dataclasses import dataclass, field
 
 
+@dataclass
 class POS:
-    def __init__(self, wikitext: Optional[str]=None):
-        if wikitext is not None:
-            self.parse(wikitext)
-        self.wikitext = None
+    wikitext: str
+    pos_class: str = field(init=False)
 
-    def parse(self, wikitext: str):
-        pass
+    def __post_init__(self):
+        self.pos_class = self.parse_pos()
+
+    def parse_pos(self) -> str:
+        text = self.wikitext.strip("'\n")
+        if text == '':
+            pos = 'None'
+        elif 'rzeczownik' in text:
+            pos = 'noun'
+        elif 'czasownik' in text:
+            pos = 'verb'
+        elif 'przymiotnik' in text:
+            pos = 'adjective'
+        elif 'przysłówek' in text:
+            pos = 'adverb'
+        elif 'spójnik' in text:
+            pos = 'conjunction'
+        elif 'zaimek' in text:
+            pos = 'pronoun'
+        elif 'przyimek' in text:
+            pos = 'preposition'
+        elif 'wykrzyknik' in text:
+            pos = 'interjection'
+        elif 'partykuła' in text:
+            pos = 'particle'
+        elif 'liczebnik' in text:
+            pos = 'numeral'
+        else:
+            pos = 'other'
+        return pos
+
+
+@dataclass
+class Sense:
+    id: str
+    wikitext: str
+    pos: POS
