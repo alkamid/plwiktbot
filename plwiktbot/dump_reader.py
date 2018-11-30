34d70913cd1346969facfe585ea7d7f4e238ca93
alkamid <adamkli@gmail.com>
2018-11-30 00:13:31 +0000

    Docs and refactoring of dump reading functions

--- plwiktbot/dump_reader.py
+++ plwiktbot/dump_reader.py
@@ -1,8 +1,8 @@
 import json
 import bz2
-from os import unlink
+import os
 from tempfile import mkstemp
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Tuple
 
 
 from pywikibot.xmlreader import XmlDump, XmlEntry
@@ -48,46 +48,80 @@ def read_dump(filename: str):
             pass
 
 
-def build_index_dict(index_filename: str) -> Dict[str, List[int]]:
-    # TODO: Last 100 pages have None as end index. Change?
-    in_dict = {}
-    with bz2.BZ2File(index_filename) as f:
-        index_text = f.read().decode('utf-8')
-
+def build_multistream_index_dict(index_filename: str) -> Dict[str, Tuple[int, int, int]]:
+    """
+    Take a multistream index file and build a look-up dictionary for each page.
+    Args:
+        index_filename:
+    Returns:
+        Dict of page -> (page id in a chunk, starting byte of the chunk, ending byte of the chunk)
+        The last element of the tuple is -1 for the last chunk of bzipped multistream dump.
+    """
+    index_dict = {}
     indices = []
-    for i, line in enumerate(index_text.split('\n')):
-        lsp = line.split(':', maxsplit=2)
-        if not lsp[0]:
-            continue
-        in_dict[lsp[2]] = [i % 100, int(lsp[0]), None]
-        if i % 100 == 0:
-            indices.append(int(lsp[0]))
+    with bz2.open(index_filename, 'rt', encoding='utf-8') as f:
+        for i, line in enumerate(f):
+            lsp = line.split(':', maxsplit=2)
+            start_index = lsp[0]
+            if not start_index:
+                continue
+            page_title = lsp[2].strip()
+            index_dict[page_title] = (i % 100, int(start_index), -1)
+            if i % 100 == 0:
+                indices.append(int(start_index))
 
-    for i, (key, val) in enumerate(in_dict.items()):
+    for i, (key, val) in enumerate(index_dict.items()):
         try:
-            in_dict[key][2] = indices[i // 100 + 1]
+            index_dict[key] = (*index_dict[key][:2], indices[i // 100 + 1])
         except IndexError:
             pass
 
-    return in_dict
+    return index_dict
 
 
-def lookup_page(title: str, index_dict: Dict, filename_dump: str) -> XmlEntry:
-    id, start, stop = index_dict[title]
-    len_header = 670
+def lookup_page(title: str, filename_dump: str, index_dict: Optional[Dict]=None) -> XmlEntry:
+    """
+    Look up a page in a multistream dump. If the page index is loaded into memory and given as
+    index_dict, this function returns a page with a given title from an XML dump in a matter of 10-30ms.
+    Args:
+        title: Title of the page to be looked up
+        filename_dump:
+        index_dict: Optional, but recommended. A pre-build index of where particular titles are in
+        the multistream dump. If not given, we'll try to do a naive search for the proper file.
+
+    Returns:
+        XmlEntry of the desired page
+    """
+    if index_dict is None:
+        print('Looking up the index file... (for better performance, build the index dictionary first,'
+              ' and then look up pages)')
+        index_path = str(filename_dump).replace('.xml.bz2', '-index.txt.bz2')
+        try:
+            index_dict = build_multistream_index_dict(index_path)
+        except FileNotFoundError:
+            raise FileNotFoundError(f'Automatic lookup of the index file failed!\nFile not found: {index_path}\n'
+                                    'Make sure the index is in the same directory as dump or provide an already'
+                                    'built index mapping as the third argument to lookup_page()')  # TODO: replace lookup_page() with auto generated name of the function
+
+    id_, start, stop = index_dict[title]
+    len_header = list(index_dict.values())[0][1]  # the first element in the dict starts at the "header" of "offset"
     with open(filename_dump, 'rb') as f:
         header = f.read(len_header)
         f.seek(start)
-        compressed_100_pages = f.read(stop-start)
+        if stop != -1:
+            compressed_100_pages = f.read(stop-start)
+        else:
+            compressed_100_pages = f.read()
+        tempfile_handle, tempfile = mkstemp(suffix='.bz2')
         try:
-            _, tempfile = mkstemp(suffix='.bz2')
             with open(tempfile, 'wb') as g:
                 g.write(header + compressed_100_pages)
             gen = XmlDump(tempfile).parse()
             for i, page in enumerate(gen):
-                if i == id:
+                if i == id_:
                     return page
         finally:
-            unlink(tempfile)
+            os.close(tempfile_handle)
+            os.unlink(tempfile)
 
 
